18b1f901208588780da791c78a361a9655aa02ee
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-18 16:06:00 +0530

    Update A4. Fibonacci Numbers till N.js

--- A1. JavaScript/A1. Getting Started/A4. Fibonacci Numbers till N.js
+++ A1. JavaScript/A1. Getting Started/A4. Fibonacci Numbers till N.js
@@ -1,3 +1,26 @@
+Take as input n. Determine all prime numbers till n and print them on the same line with 
+spaces in between
+Input Format
+
+Integer
+
+Constraints
+
+n <= 10 ^ 9
+
+Output Format
+
+Space separated integers on the same line
+
+Sample Input 0
+
+7
+Sample Output 0
+
+2 3 5 7
+
+Code:
+
 function fibo(n){
 
     var a=0;
