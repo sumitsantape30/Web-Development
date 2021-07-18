897a0f3da2783e9d4f36528089f25c0d944510a4
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-18 15:53:34 +0530

    Create A3. FixxBuzz.js

---
+++ A1. JavaScript/A1. Getting Started/A3. FixxBuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz(n){
+
+    for( var i=1; i <= n; i++){
+
+        if( i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz");
+        }else if( i % 5 == 0){
+            console.log("Buzz");
+        }else if( i % 3 == 0){
+            console.log("Fizz");
+        }else{
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz(50);
