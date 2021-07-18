3064fb9731eab8f13f5d269c7d41ddfff676e75c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-18 16:05:19 +0530

    Update A3. FixxBuzz.js

--- A1. JavaScript/A1. Getting Started/A3. FixxBuzz.js
+++ A1. JavaScript/A1. Getting Started/A3. FixxBuzz.js
@@ -1,3 +1,41 @@
+You are given a number starting from 1 to that number you need to print for multiples of
+
+'3' print "Fizz"
+'5' print "Buzz" '3' and '5' both print "FizzBuzz" else number itself
+
+Input Format
+
+A number n
+
+Constraints
+
+2 <= n < 10^9
+
+Output Format
+
+Fizz
+Buzz
+FizzBuzz
+number
+Sample Input 0
+
+10
+Sample Output 0
+
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+
+
+Code:
+
 function fizzbuzz(n){
 
     for( var i=1; i <= n; i++){
