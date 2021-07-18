682ca2dab74913620e5d3ce7058cefae087217d4
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-18 08:39:40 +0530

    Create A1. Intro.js

---
+++ A1. JavaScript/A1. JS Intro/A1. Intro.js
@@ -0,0 +1,29 @@
+//no main -> isme main nhi hota
+// left to right and top to bottom code run hota hai
+console.log("Hello World") // print karne keliye
+
+//variable declare, aur datatype dene ki jarurat nhi hoti
+let a;
+// variables ki default value hoti hai 'undefined'
+
+// isme only numbers hote hai ->  integers/float nhi hote hai
+a= 10;
+//console.log(5/2); //2.5 dega output
+
+//only String hoti hai -> characters nhi hote
+a= "I am String";
+a= 'I am also string';
+
+// boolean -> true/false
+a= true;
+// null
+a= null;
+console.log(a);
+
+//summary: variable -> declare default -> undefined
+// primitive types js:  undefined, numbers, string, boolean, null
+
+// loops, classes, conditional are similarto java
+for( let i=1 ; i<= 10; i++){
+    console.log("Number is "+i);
+}
