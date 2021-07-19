23d64bfaf700ebdcff3fc8e661390f50a1171392
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-19 13:08:21 +0530

    Create A7. Arrays_Union.java

---
+++ A1. JavaScript/A2. Input, String & Arrays/A7. Arrays_Union.java
@@ -0,0 +1,25 @@
+Write a JavaScript program to compute the union of two arrays . Input array will only have unique elements in there respective array. Input arr1= [1, 2, 3] arr2= [100, 2, 1,10] Output: [1,2,3,100,10]
+
+Input Format
+
+arr1= [1, 2, 3] arr2= [100, 2, 1,10]
+
+Constraints
+0<=arr1.length < 10^4 0<=arr2.length < 10^4
+
+Output Format
+
+[1,2,3,100,10]
+
+Sample Input 0
+
+[1, 2, 3] 
+[100, 2, 1,10]
+Sample Output 0
+
+[ 1, 2, 3, 100, 10 ]
+
+Code:
+
+   
+ 
