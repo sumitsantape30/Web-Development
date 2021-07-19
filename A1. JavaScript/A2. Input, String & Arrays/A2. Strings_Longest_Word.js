35f242072e35260b6572c00c555b2883db8ee389
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-19 08:45:36 +0530

    Create A2. Strings_Longest_Word.js

---
+++ A1. JavaScript/A2. Input, String & Arrays/A2. Strings_Longest_Word.js
@@ -0,0 +1,38 @@
+Print the Longest Word in a String
+
+Input Format
+
+string
+
+Constraints
+
+''
+
+Output Format
+
+String
+
+Sample Input 0
+
+The quick brown fox jumped over the lazy dog
+Sample Output 0
+
+jumped
+
+Code:
+
+function longestWord(str){
+
+    let arrStr= str.split(" ");
+    var max= 0;
+    var ans= "";
+
+    for( var i =0; i< arrStr.length; i++){
+
+        if( arrStr[i].length() > max){
+            max= arrStr[i].length;
+            ans= arrStr[i];
+        }
+    }
+    console.log(ans);
+}
