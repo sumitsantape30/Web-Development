6f4447900c297343271e39a8a4a36c61e384205b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-19 21:05:32 +0530

    Update A7. Arrays_Union.java

--- A1. JavaScript/A2. Input, String & Arrays/A7. Arrays_Union.java
+++ A1. JavaScript/A2. Input, String & Arrays/A7. Arrays_Union.java
@@ -21,5 +21,36 @@ Sample Output 0
 
 Code:
 
+function union(str){
+
+    str = str.split("\n");
+    let a1= JSON.parse(str[0]);
+   // console.log(a1);
+    let a2= JSON.parse(str[1]);
+  //  console.log(a2);
+    let ans= [];
+
+    let i=0;
+    while( i < a1.length){
+        ans.push(a1[i]);
+        i++;
+    }
+
+    console.log(ans);
+
+    let j=0;
+    let len= a2.length;
+    while( j < len){
+        if(! ans.includes(a2[j])){
+            ans.push(a2[j]);
+            //console.log(ans);
+        }
+        j++;
+    }
+    console.log(ans);
+}
+
+union("[1, 2, 3]\n[100, 2, 1,10]");
+
    
  
