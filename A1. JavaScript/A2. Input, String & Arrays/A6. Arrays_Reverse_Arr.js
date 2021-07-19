e888954defd6bbe649ce01b32fe5ff33bf9b4e03
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-19 10:31:02 +0530

    Create A6. Arrays_Reverse_Arr.js

---
+++ A1. JavaScript/A2. Input, String & Arrays/A6. Arrays_Reverse_Arr.js
@@ -0,0 +1,38 @@
+Given an array first=[1,2,3,4,5]. Write a JavaScript program that fill second array in reverse order of first narr=[5,4,3,2,1] without using extra space
+
+Input Format
+
+Array
+
+Constraints
+
+0<=arr.length< 10^4
+
+Output Format
+
+Array
+
+Sample Input 0
+
+[1,2,3,4,5]
+Sample Output 0
+
+[ 5, 4, 3, 2, 1 ]
+
+Code:
+
+function reverse(strArr){
+
+    let arr= JSON.parse();
+
+    let left=0;
+    let right= arr.length-1;
+    while( left < right){
+        let temp= arr[left];
+        arr[left]= arr[right];
+        arr[right]= temp;
+        left++;
+        right--;
+    }
+    console.log(arr);
+}
