0b0366da4497b8b7657ff9f5026949554841b46a
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-19 08:57:41 +0530

    Create A3. Strings_Title_case.js

---
+++ A1. JavaScript/A2. Input, String & Arrays/A3. Strings_Title_case.js
@@ -0,0 +1,22 @@
+Title Case a Sentence. Input:I'm a little tea pot" Output: I'm A Little Tea Pot.
+
+Input Format
+
+String
+
+Constraints
+
+no as such
+
+Output Format
+
+String
+
+Sample Input 0
+
+I'm a little tea pot
+Sample Output 0
+
+I'm A Little Tea Pot
+
+Code:
