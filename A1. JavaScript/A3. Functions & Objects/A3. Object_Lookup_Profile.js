36b8f06a2663792b5c49dcbc1b74712fbec1c29c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-20 10:44:25 +0530

    Update A3. Object_Lookup_Profile.js

--- A1. JavaScript/A3. Functions & Objects/A3. Object_Lookup_Profile.js
+++ A1. JavaScript/A3. Functions & Objects/A3. Object_Lookup_Profile.js
@@ -11,3 +11,27 @@ If name does not correspond to any contacts then return the string No such conta
 
 If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
 
+Code:
+
+function lookUpProfile(name, prop) {
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+
+Code Explanation:
+
+The for loop runs, starting at the first object in the contacts list.
+If the firstName parameter passed into the function matches the value of the "firstName" key in the first object, the if statement passes.
+Then, we use .hasOwnProperty() method (checks if there’s a given property and returns a boolean) with prop as an argument. If it’s true, the value of prop is returned.
+If the second if statement fails, No such property is returned.
+If the first if statement fails, the for loop continues on to the next object in the contacts list.
+If the firstName parameter isn’t matched by the final contacts object, the for loop exits and No such contact is returned.
