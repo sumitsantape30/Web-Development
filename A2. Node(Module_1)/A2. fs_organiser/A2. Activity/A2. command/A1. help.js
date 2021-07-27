6d9ccae710b1f98c2bd9e531411a7a6c8de50741
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-27 10:26:07 +0530

    Create A1. help.js

---
+++ A2. Node(Module_1)/A2. fs_organiser/A2. Activity/A2. command/A1. help.js
@@ -0,0 +1,10 @@
+function helpfn(){
+    console.log(`List of all the commands :
+                  1. node main.js tree "path"
+                  2. node main.js organize "path"
+                  3. node main.js help`)
+}
+
+module.exports = {
+    helpfxn= helpfn
+}
