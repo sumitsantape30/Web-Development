f39687ee4ca1256520b6463958739624e2bf684d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-09 07:13:14 +0530

    Create A1. Last Ball Commentary.js

---
+++ A1. JavaScript/A4. Web_Scrap/A1. Raw/A3. poc/A2. cricinfo/A1. Last Ball Commentary.js
@@ -0,0 +1,35 @@
+//hume input mai ek url milega crickinfo ke site ka, hume last ball pe kya commentary hui thi yeh return karna hai
+//request wala hi code yha paste karo, code same hi rehne wala hai bas url alag se dena hai
+
+// npm i request
+let request= require("request");
+let ch  = require("cheerio");
+//data extract-> cheerio
+
+let url= "https://www.espncricinfo.com/series/ipl-2020-21-1210595/sunrisers-hyderabad-vs-royal-challengers-bangalore-3rd-match-1216534/ball-by-ball-commentary";
+
+console.log("Before"); 
+request(url, cb);  //isme url pass karenge
+function cb(error, response, html) {
+    //console.error('error:', error); // Print the error if one occurred
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+    if( error){
+        console.log(error); 
+    }else if(response.statusCode == 404){
+        console.log("Page Not Found");
+    }
+    else{
+       // console.log("html:", ) 
+       dataExtractor(html)
+    }
+  }
+
+  function dataExtractor(html){ 
+    let searchTool= cheerio.load(); 
+    let elemRepArr= searchTool(".match-comment-wrapper .match-comment-long-text"); //hume bas searchtool ke andar selector pass karna hai, aur element representative mil jayega. hume waha koi bhi unique elector nhi mila so woh array deta hai coz multiple chize mili hai
+   // console.log(elemRepArr.length);
+   //cram this-> jab apko iss searchTool se array mil rha hota hai, so aap is array ke kisi element ko access krte ho to apke paske sare functiona kho jate hai to apko dubara isko searchTool ke andar pass karna pdta hai
+   let lbc= searchTool(elementArr[0]).text();
+   console.log("last ball commentary", lbc);
+}
+  console.log("After");
