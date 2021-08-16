1b8b1fd5d21157d01f3b1f7edece0df9a16089b8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-16 08:41:54 +0530

    Create A7. async_parllel.js

---
+++ A1. JavaScript/A4. Web_Scrap/A1. Raw/A6. Async (Par-Ser Reading)/A7. async_parllel.js
@@ -0,0 +1,12 @@
+let fs= require("fs");
+let files= ["./f1.txt", "./f2.txt", "./f4.txt", "./f5.txt"];
+// parallerly read using ans asyn function
+//start the work
+// also give us a cb function from which we can intimate you after the work is completed
+for(let i=0; i < files.length; i++){
+    fs.readFile(files[i], cb);
+}
+
+function cb(err, data){
+    console.log("Content: "+data);
+}
