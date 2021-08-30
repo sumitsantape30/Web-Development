bcf18c88e59f398f4f7d0a9b8f232c24cf412926
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-30 06:25:39 +0530

    Update A7. async_parllel.js

--- A1. JavaScript/A4. Web_Scrap/A1. Raw/A6. Async (Par-Ser Reading)/A7. async_parllel.js
+++ A1. JavaScript/A4. Web_Scrap/A1. Raw/A6. Async (Par-Ser Reading)/A7. async_parllel.js
@@ -11,3 +11,22 @@ function cb(err, data){
     console.log(data.byteLength); //ek file ke andar kitna content hai 
     console.log("Content: "+data);
 }
+
+//=======================================================================================
+
+let fs= require("fs");
+let files= ["./f1.txt", "./f2.txt", "./f4.txt", "./f5.txt"];
+// parallerly read using ans asyn function
+//start the work
+// also give us a cb function from which we can intimate you after the work is completed
+console.log("before");
+
+for(let i=0; i < files.length;){
+    fs.readFile(files[i], function cb(err, data){
+        console.log("data: "+data);
+        i++;
+    });
+}
+console.log("after");
+
+/* yeh sirf before print karega, aur loop enter krte hi i ki value 0 hi rahegi woh kabhi loop exit hi nhi karega */
