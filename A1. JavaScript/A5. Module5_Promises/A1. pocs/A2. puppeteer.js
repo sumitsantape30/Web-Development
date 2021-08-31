29ca7bf62ecb20493749b592c7c65454f6b43fdc
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-31 08:40:34 +0530

    Create A2. puppeteer.js

---
+++ A1. JavaScript/A5. Module5_Promises/A1. pocs/A2. puppeteer.js
@@ -0,0 +1,3 @@
+//puppeteer ek npm ka module hai jo browser ko control krta hai, chromium browser 
+//chromium browser ke andar 2 parts hote hai, UI and dev tools. so yeh puppeteer script ke thorugh iss chromium browser ko control krta hai, woh sare kam kr skta hai jo humans dev tools mai kr skta hai
+
