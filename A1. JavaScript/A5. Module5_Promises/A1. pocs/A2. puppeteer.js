63864a904f0c1578e25652f6d38112ff6f88b54e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-31 09:37:29 +0530

    Update A2. puppeteer.js

--- A1. JavaScript/A5. Module5_Promises/A1. pocs/A2. puppeteer.js
+++ A1. JavaScript/A5. Module5_Promises/A1. pocs/A2. puppeteer.js
@@ -1,3 +1,134 @@
 //puppeteer ek npm ka module hai jo browser ko control krta hai, chromium browser 
 //chromium browser ke andar 2 parts hote hai, UI and dev tools. so yeh puppeteer script ke thorugh iss chromium browser ko control krta hai, woh sare kam kr skta hai jo humans dev tools mai kr skta hai
 
+// npm i puppeteer;
+let puppeteer = require("puppeteer");
+
+//puppeteer.launch({headless: false}); //itne se browser open hojata hai 
+
+/* 
+//by default headless false hota hai
+let browserStartPromise = puppeteer.launch({}); //yeh promise dega browser open hone ka
+browserStartPromise.then(function () {
+        console.log("Browser opened");
+}) */
+
+// headless means browser dikhega nhi, sare kam hojayenge but browser dikhega nhi
+// creates headless browser
+let browserStartPromise = puppeteer.launch({ //yeh promise dega browser open hone ka
+    // visible 
+    headless: false, // headless se browser visible rahega
+    // type 1sec 
+    //slowMo: 1000,
+    defaultViewport: null,
+    // browser setting 
+    args: ["--start-maximized", "--disable-notifications"]
+});
+
+/*
+browserStartPromise.then(function (browserObj) {
+            console.log("Browser opened");
+            let browserTabOpenPromise = browserObj.newPage(); //yeh function browser ke andar ek naya tab khol dega
+            browserTabOpenPromise.then(function(page)){
+             console.log("new tab opened");
+        })
+    })
+    // yahatk code se chrome open hoga aur new blank tab open hoga
+
+    //agar muje ek ke bad ek serially kam karna hai to then ke andar then aise kr skte hai, same code for above operation
+    // koi banda promise deta hai use return karo aur fir uska jo consumer hai ki jab woh resolve hojayega to woh uske niche likhenge
+    browserStartPromise.then(function (browserObj) {
+        console.log("Browser opened");
+        // new tab 
+        let browserTabOpenPromise = browserObj.newPage(); 
+        return browserTabOpenPromise;
+    }).then(function (page) {
+        console.log("new tab opened ")
+        let gPageOpenPromise = newTab.goto("https://www.google.com/");
+        return gPageOpenPromise;
+    }).then(function () {
+        console.log("Google home page opened");
+    });
+ */
+
+let page, browser, rTab;
+browserStartPromise.then(function (browserObj) {
+        console.log("Browser opened");
+        // new tab 
+        browser = browserObj
+        let browserTabOpenPromise = browserObj.newPage(); 
+        return browserTabOpenPromise;
+    }).then(function (newTab) {
+        page = newTab;
+        console.log("new tab opened ")
+        let gPageOpenPromise = newTab.goto("https://www.google.com/");
+        return gPageOpenPromise;
+    }).then(function () {
+        console.log("Google home page opened");
+        // keyboard se -> data entry karne keliye
+        let waitforTypingPromise = page.type("input[title='Search']", "pepcoding");
+        return waitforTypingPromise;
+    }).then(function () {
+        // keyboard se-> specific keys press karne kam ayega
+        let enterWillBeDonePromise = page.keyboard.press('Enter', { delay: 100 });
+        return enterWillBeDonePromise;
+    }).then(function () { 
+        // next page 
+        //wait for element to be visible on the page-> whenver you go to a new page 
+        console.log("wait for element to be visible");
+        let waitForElementPromise = page.waitForSelector(".LC20lb.DKV0Md", { visible: true });//jabtak yeh element visible na ho aap wait karoge
+        return waitForElementPromise;
+    }).then(function () {
+        
+        // mouse function 
+        let elemClickPromise = page.click(".LC20lb.DKV0Md");
+        return elemClickPromise;
+    }).then(function () {
+
+        // 30 seconds 
+        let waitForModalPromise = page.waitForSelector("#lp_modal_close", { visible: true }); //jabtak woh poster visible na ho aap wait karoge
+        return waitForModalPromise;
+    }).then(function () {
+
+        let clickModal = page.click("#lp_modal_close", { delay: 100 });// uspe click karwa diya
+        return clickModal;
+    }).then(function () {
+        // page element -> cheerio 
+         //page.$$ function ke andar selector pass karenge to yeh elements ka array lakee dega 
+        let allLisPromise = page.$$(".site-nav-li"); // resources open karna hai hume 
+        return allLisPromise;
+
+    }).then(function (allElem) {
+
+        let elementWillBeclickedPromise = allElem[6].click({ delay: 100 }); // sabhi elements the usme 6th element pe click karenge
+        return elementWillBeclickedPromise; 
+    }).then(function () { // resources page is on next tab and next tab will take some time to open 
+        
+        let future2secondAfter = Date.now() + 2000;
+        while (Date.now() < future2secondAfter) {
+        }
+        let listofOpenedTabsPromise = browser.pages(); // jo open tab hai uska promise hai yeh
+        return listofOpenedTabsPromise;
+    }).then(function (array) {
+
+        rTab = array[array.length - 1]; //rTab upar initialise kiye usme dal rhe hai
+        let waitforLevel1Promise = rTab.waitForSelector('h2[title="Data Structures and Algorithms in Java [Level 1 - Foundation]"]', { visible: true });
+        return waitforLevel1Promise;
+    }).then(function () {
+
+        let clickLevel1Promise = rTab.click('h2[title="Data Structures and Algorithms in Java [Level 1 - Foundation]"]');
+        return clickLevel1Promise;
+    }).then(function () {
+
+        console.log("level 1 will be opened");
+    })
+
+    // browser.pages -> array of all the open tabs
+
+    // .then(function (array) {
+    //     rTab = array[array.length - 1];
+    //     let urlPromise = rTab.url();
+    //     return urlPromise
+    // }).then(function (url) {
+    //     console.log(url)
+    // })
