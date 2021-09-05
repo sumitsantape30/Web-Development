3211fdc1b9e5740393d57987e8b1c4f837d81a16
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-05 06:30:02 +0530

    Create A3. CreatePromise.js

---
+++ A1. JavaScript/A5. Module5_Promises/A1. pocs/A1. facts/A3. CreatePromise.js
@@ -0,0 +1,36 @@
+//promises hum khud bana bhi skte hai 
+// muje myPromisiedFsReader iss function ko aise build karna hai taki yeh promise ki tarah kam kare
+// jaise fs.promise.readFile kam krta tha waisehi myPromisiedFsReader iss function ko likhna chahte hai
+
+// /implement -> polyfill 
+// fs.promises.readFile ka humne polyfill likha hai
+let fs = require("fs");
+function myPromisiedFsReader(filePath) {
+    // using this existing function
+    return new Promise(function (resolve, reject) { //agar apko khudka promises bade function return/create karna hai to usko hum return new Promise ke andar put kardete hai
+        fs.readFile(filePath, function cb(err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data)
+            }
+        })
+
+    })
+}
+
+// consumer 
+console.log("Before");
+let freadPromise = myPromisiedFsReader("f1.txt");
+console.log("promise", freadPromise);
+// promise -> resolve -> data 
+// function pass -> resolve 
+freadPromise.then(function cb(data) {
+    console.log("data " + data);
+})
+// function pass -> reject 
+freadPromise.catch(function cb(err) {
+    console.log("error " + err);
+})
+console.log("After");
