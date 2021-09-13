1a245996e8abef95df61579308b94a0f482b5491
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-13 17:36:40 +0530

    Update A5. Provider.js

--- A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A5. Provider.js
+++ A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A5. Provider.js
@@ -1,12 +1,12 @@
 let fs = require('fs');
 // request 
-let axios = require("axios");
+let axios = require("axios"); //axios request marne ke kam ata hai
 // file with name f1 will be read
 let pPromise = fs.promises.readFile("f1.txt");
 // Hello will be written into file f2 
 fs.promises.writeFile("f2.txt", "Hello")
 // 
-let respPromise = axios.get("https://jsonplaceholder.typicode.com/users");
+let respPromise = axios.get("https://jsonplaceholder.typicode.com/users"); //app URL pass karke data mangwa skte ho
 pPromise.then(function (data) {
     console.log("Result of fileReadPromise: " + data);
 })
