6af3d1e5238e3c93fc2b95f35235bf9ef33e1d0d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-13 19:44:01 +0530

    Update A4. Promises.js

--- A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A4. Promises.js
+++ A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A4. Promises.js
@@ -1,6 +1,6 @@
-// This code prooves 2 points of promisesfacts.md:> Promise => assurance of a work that could be completed in future.
+// This code prooves points of promisesfacts.md:> Promise => assurance of a work that could be completed in future.
 // promise-> initial state-> pending, value -> undefined final state-> settled resolved -> you have got the future value rejected-> error
-// Open book for theory and better understanding
+// Open book and google doc for theory and better understanding
 
 let fs = require("fs");
 function myPromisiedFsReader(filePath) {
