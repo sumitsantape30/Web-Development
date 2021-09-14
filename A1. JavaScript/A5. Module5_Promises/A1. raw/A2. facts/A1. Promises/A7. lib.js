ba0f7615ca66dcfba2536a482ead9c2f995ce3eb
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-14 16:46:32 +0530

    Update A7. lib.js

--- A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A7. lib.js
+++ A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A7. lib.js
@@ -11,14 +11,14 @@ function analyzedata(product, cb) {
 //instead of analyzedata, humara pas promisifiedAnalyzedata function hai, promisified function mai yeh khas bat hoti hai ki apko bas data pass karna hota hai callback then mai pass hota hai 
 function promisifiedAnalyzedata(product) {
     return new Promise(function (resolve, reject) {
-        setTimeout(function () {
+        setTimeout(function () { //yaha maine resolve 3 bar lagaya fir bhi woh ek hi bar chalega. so promises can be resolved/ rejected once in a lifetime.
             // kdjdhvbjdhvbbv
             // dfdvjdfbvjhdf
             resolve();
             resolve();
             reject();
         }, 2000);
-    })
+    }) 
 }
 module.exports = {
     analyzedata: analyzedata,
