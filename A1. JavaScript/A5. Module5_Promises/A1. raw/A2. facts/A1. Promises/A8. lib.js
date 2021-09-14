b75e7f9cac8a46a128e64da1653cda2c870e1f05
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-14 16:38:08 +0530

    Update A8. lib.js

--- A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A8. lib.js
+++ A1. JavaScript/A5. Module5_Promises/A1. raw/A2. facts/A1. Promises/A8. lib.js
@@ -6,7 +6,9 @@ function analyzedata(product, cb) {
         cb();
         cb();
     }, 2000);
-}
+}//upar wale ko comment krdena
+
+//instead of analyzedata, humara pas promisifiedAnalyzedata function hai, promisified function mai yeh khas bat hoti hai ki apko bas data pass karna hota hai callback then mai pass hota hai 
 function promisifiedAnalyzedata(product) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
