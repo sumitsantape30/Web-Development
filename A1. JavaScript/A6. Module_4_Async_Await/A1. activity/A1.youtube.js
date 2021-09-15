163c217212598716640233a940121579360c81c3
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-15 07:17:07 +0530

    Create A1.youtube.js

---
+++ A1. JavaScript/A6. Module_4_Async_Await/A1. activity/A1.youtube.js
@@ -0,0 +1,17 @@
+//We are doing this activity usig Async, Await
+
+//we have to find the following this,
+
+// a. Name of Playlist,view
+// b. Total No of videos : 792
+// c. actual No of videos :783
+// d. Total length of playlist : 12 hours, 9 minutes, 12 seconds
+// At 1.25x : 9 hours, 43 minutes, 21 seconds
+// At 1.50x : 8 hours, 6 minutes, 8 seconds
+// At 1.75x : 6 hours, 56 minutes, 41 seconds
+// At 2.00x : 6 hours, 4 minutes, 36 seconds
+// Average length of video : 29 minutes, 10 seconds
+
+// e. console.table=>  of video number,name,time
+
+
