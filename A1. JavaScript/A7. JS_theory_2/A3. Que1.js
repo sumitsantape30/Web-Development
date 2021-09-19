c8ccc69ff5e390b8bd25b34fb775307eaf01aad7
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-19 08:45:44 +0530

    Update A3. Que1.js

--- A1. JavaScript/A7. JS_theory_2/A3. Que1.js
+++ A1. JavaScript/A7. JS_theory_2/A3. Que1.js
@@ -55,8 +55,25 @@ Output:
 2 3
 
 //================================================================
-// agar apko 0 1 2 hi print karna hai but without call function inside the loop
+//agar humne loop ke andar var ki jagah let use kiya 
+// let ki case mai closure nhi banta apkse jo values hoti hai woh block se mil jati hai
 
+function outer() {
+    let arrFn = [];
+    // local -> 3
+    // let i = 0
+    // 
+    // block 0,block 1 ,block 2
+    for (let i=0; i < 3; i++) {
+        // block scope
+        arrFn.push(function fn() {
+            console.log(i);
+        })
+    }
+    return arrFn;
+}
+let arrFn = outer();
 
-
-
+arrFn[0]();
+arrFn[1]();
+arrFn[2]();
