1099a278262deb7b75852196fd2fed74937c05f5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-07-28 15:37:03 +0530

    Create A8. LocalStorage.js

---
+++ JavaScript Tutorial (CWH)/A8. LocalStorage.js
@@ -0,0 +1,6 @@
+
+//Local storage, every webpage/website has its own local storage
+//inspect and open console
+localStorage.setItem('Name', 'Sumit'); // isse browser ke local storage ke andar name sumit store hojayega
+localStorage; // to view to localStorage
+localStorage.clear(); // to clear the localstorage of current page
